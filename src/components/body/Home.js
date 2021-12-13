7b171465ab19c1691b0deda93b8a2f0f8ee98802
Ehsanul Islam Sajjad <ehsanulislam82@gmail.com>
2021-12-13 14:05:39 +0600

    add --openssl-legacy-provider in package.json

--- src/components/body/Home.js
+++ src/components/body/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Home extends Component {
   render() {
-    document.title = 'Rajmohol';
+    document.title = 'Welcome To Rajmohol';
 
     return <div>this is home</div>;
   }
